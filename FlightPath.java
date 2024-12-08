e6c2588a573f1a539979d75d0958142e301f0efd
dylandray <120407558+dylandray@users.noreply.github.com>
2024-12-09 00:33:55 +0100

    Update FlightPath.java
    added functions to send details to the google map API (still need to figure out how to have the API working) / added function to put the string into a csv format separated by commas

--- FlightPath.java
+++ FlightPath.java
@@ -18,7 +18,7 @@ public class FlightPath
 		time = record.get(0);
 		latitude = record.get(1);
 		longitude = record.get(2);
-		airport = record.getLast();
+		airport = record.get(record.size() - 1);
 		
 		// Getting airport code from airport string by searching for parentheses
 		if(!airport.equals(" Surface and Near-Surface"))
@@ -54,17 +54,43 @@ public class FlightPath
 			airportCode.append("N/A"); // No airport code provided
 		}
 		
-		// call function to get flight location
-		// call function to get airport information
-		
+		fetchLocation(); // Fetch location from GMaps
+		fetchAirport(); // Fetch airport name & location from GMaps
 	}// end Constructor
 	
 	
-	// TODO: Create function to send latitude & longitude to Google API to retrieve flight location
-	
-	// TODO: Create function to send airport code to Google API to get airport name & location
-	
-	
+	// function to send latitude and longitude to Google API to retrieve flight location
+	private void fetchLocation() {
+		try {
+			// put the API call to Google Maps once found
+			location = "Town, State"; // Example location
+		} catch (Exception e) {
+			System.out.println("Unable to retrieve location from Google Maps API.");
+			location = "unknown";
+		}
+	}
+	// function to send airport code to Google API to get airport name & location
+	private void fetchAirport() {
+		try {
+			if (!airportCode.toString().equals("N/A")) // If airport code is available
+			{
+				// put the API call to Google Maps once found
+				airportName = "Airport Name"; // Example airport name
+				airportLocation = "city, country"; // Example airport location
+			}
+			else {
+			airportName = "unknown";
+			airportLocation = "unknown";
+			}
+		} catch (Exception e) {
+			System.out.println("Unable to retrieve airport information from Google Maps API.");
+			airportName = "unknown";
+			airportLocation = "unknown";
+			}
+	}
+	public String toCSVstring() { // Return a string in CSV format(separated by commas)
+		return String.format("%s,%s,%s,%s,%s,%s,%s", time, latitude, longitude, location, airportCode.toString(), airportName, airportLocation);
+	}
 	public void print() // TODO: Create string to send to file instead of printing
 	{
 		System.out.printf("Time: %s\tLatitude: %s\tLongitude: %s\tCurrent Location: %s\tAirport Code: %s\tAirport Name: %s\tAirport Locale: %s%n",
